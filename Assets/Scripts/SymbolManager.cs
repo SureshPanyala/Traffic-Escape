c78332d68164bc5b2ff6d348a36a945305c2b0d8
Mallik Ebadat <31974569+MK-IB@users.noreply.github.com>
2023-12-08 12:15:48 +0530

    Revert "Merge branch 'main' into Malik-Branch"
    This reverts commit 4b0f555a53ed73d1b06c9b0aaff477f00906c57c, reversing
    changes made to 4a8fe052144093269122c2272b48a3bc045f4e52.

--- Assets/Scripts/SymbolManager.cs
+++ Assets/Scripts/SymbolManager.cs
@@ -5,11 +5,18 @@ public static class SymbolManager
 {
     public enum SymbolsEnum
     {
-        Right,
-        Left,
-        Straight,
-        UturnLeft,
-        UTurnRight
+        leftTurn,
+        rightTurn,
+        straight,
+        lowLeft,
+        lowRight,
+        hardLeft,
+        hardRight,
+        rightUTurn,
+        leftUTurn,
+        whiteStraight,
+        whiteLeft,
+        whiteUturnLeft,
     }
 
     private static Dictionary<SymbolsEnum, Sprite> symbolDictionary;
@@ -23,11 +30,18 @@ public static class SymbolManager
     {
         symbolDictionary = new Dictionary<SymbolsEnum, Sprite>
         {
-            { SymbolsEnum.Straight, Resources.Load<Sprite>("Symbols/Straight") },
-            { SymbolsEnum.Left, Resources.Load<Sprite>("Symbols/Left") },
-            { SymbolsEnum.UturnLeft, Resources.Load<Sprite>("Symbols/UturnLeft") },
-            { SymbolsEnum.Right, Resources.Load<Sprite>("Symbols/Right") },
-            { SymbolsEnum.UTurnRight, Resources.Load<Sprite>("Symbols/UTurnRight") }
+            { SymbolsEnum.leftTurn, Resources.Load<Sprite>("Symbols/leftTurn") },
+            { SymbolsEnum.rightTurn, Resources.Load<Sprite>("Symbols/rightTurn") },
+            { SymbolsEnum.straight, Resources.Load<Sprite>("Symbols/straight") },
+            { SymbolsEnum.lowLeft, Resources.Load<Sprite>("Symbols/lowLeft") },
+            { SymbolsEnum.lowRight, Resources.Load<Sprite>("Symbols/lowRight") },
+            { SymbolsEnum.hardLeft, Resources.Load<Sprite>("Symbols/hardLeft") },
+            { SymbolsEnum.hardRight, Resources.Load<Sprite>("Symbols/hardRight") },
+            { SymbolsEnum.rightUTurn, Resources.Load<Sprite>("Symbols/rightUTurn") },
+            { SymbolsEnum.leftUTurn, Resources.Load<Sprite>("Symbols/leftUTurn") },
+            { SymbolsEnum.whiteStraight, Resources.Load<Sprite>("Symbols/whiteStraight") },
+            { SymbolsEnum.whiteLeft, Resources.Load<Sprite>("Symbols/whiteLeft") },
+            { SymbolsEnum.whiteUturnLeft, Resources.Load<Sprite>("Symbols/whiteUturnLeft") }
         };
     }
 
--- Assets/Scripts/VehicleSpawner.cs
+++ Assets/Scripts/VehicleSpawner.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.ProBuilder.MeshOperations;
@@ -8,53 +7,56 @@ using UnityEngine.Splines;
 public class VehicleSpawner : MonoBehaviour
 {
 
-    [SerializeField] SplineContainer splineRoute;
-    public SplineContainer changeSplineRoute;
+    SplineContainer splineRoute;
     [SerializeField] VehicleManager.Vehicles carType;
     [SerializeField] SymbolManager.SymbolsEnum symbolsType;
-    [SerializeField] SymbolManager.SymbolsEnum ChangeSymbolsType;
     [SerializeField] Vector3 CarRotation;
-    public GameObject car;
-    SplineAnimate animate;
+    GameObject car;
     private void OnEnable()
     {
         splineRoute = GetComponent<SplineContainer>();
     }
     private void Start()
     {
-      StartCoroutine(InstantiateVehicle());
+       InstantiateVehicle();
     }
-    private IEnumerator InstantiateVehicle()
+    private void InstantiateVehicle()
     {
         car = VehicleManager.Instance.GetCarType(vehicle: carType);
   
         AddSplineAnimateToCar();
         ApplySymbolOnCar();
         car.SetActive(true);
-       // car.transform.localRotation = transform.localRotation;
+
         car.GetComponent<VehicleController>().enabled = true;
+<<<<<<< Updated upstream
+       
+=======
         yield return null;
     }
     public void SwitchSplineAnimateOfCar()
     {
-        car.GetComponent<VehicleController>().directionMarkImage.GetComponent<SpriteRenderer>().sprite = SymbolManager.GetSymbol(ChangeSymbolsType);
+        car.GetComponent<VehicleController>().directionMarkImage.sprite = SymbolManager.GetSymbol(ChangeSymbolsType);
         animate.Container = changeSplineRoute;
     }
     public void SwitchSpline()
     {
             animate.Container = changeSplineRoute;
-            car.GetComponent<VehicleController>().directionMarkImage.GetComponent<SpriteRenderer>().sprite = SymbolManager.GetSymbol(ChangeSymbolsType);
+            car.GetComponent<VehicleController>().directionMarkImage.sprite = SymbolManager.GetSymbol(ChangeSymbolsType);
             GameManager.instance.currentPower = GameManager.PowerType.none;
+            GameManager.instance.isCarMoving = false;
+>>>>>>> Stashed changes
     }
+
     private void AddSplineAnimateToCar()
     {
-        animate = car.AddComponent<SplineAnimate>();
+        SplineAnimate animate = car.AddComponent<SplineAnimate>();
         animate.PlayOnAwake = false;
         animate.Container = splineRoute;
         animate.AnimationMethod = SplineAnimate.Method.Speed;
         animate.MaxSpeed = 15;
         animate.Loop = SplineAnimate.LoopMode.Once;
-        animate.StartOffset = 0.001f;        // car.transform.rotation = Quaternion.Inverse(splineRoute.Splines.ToList()[0].Knots.ToList()[0].Rotation);
+        // car.transform.rotation = Quaternion.Inverse(splineRoute.Splines.ToList()[0].Knots.ToList()[0].Rotation);
         //Debug.Log("first knot rotaion is"+splineRoute.Splines.ToList()[0].Knots.ToList()[0].Rotation);
     }
 
@@ -62,20 +64,17 @@ public class VehicleSpawner : MonoBehaviour
     {
         if (Enum.IsDefined(typeof(SymbolManager.SymbolsEnum), symbolsType))
         {
-            car.GetComponent<VehicleController>().directionMarkImage.GetComponent<SpriteRenderer>().sprite = SymbolManager.GetSymbol(symbolsType);
+            car.GetComponent<VehicleController>().directionMarkImage.sprite = SymbolManager.GetSymbol(symbolsType);
         }
     }
 
     private void Update()
     {
-        //if (car != null && splineRoute != null && splineRoute.Splines.Count > 0)
-        //{
-        //  //  Quaternion initialRotation = Quaternion.Inverse(splineRoute.Splines.ToList()[0].Knots.ToList()[0].Rotation);
-           
-        //}
-    }
-    private void FixedUpdate()
-    {
-      //  car.transform.localRotation = Quaternion.Euler(CarRotation);
+        if (car != null && splineRoute != null && splineRoute.Splines.Count > 0)
+        {
+          //  Quaternion initialRotation = Quaternion.Inverse(splineRoute.Splines.ToList()[0].Knots.ToList()[0].Rotation);
+            car.transform.rotation = Quaternion.Euler(CarRotation);
+        }
+        // car.transform.localRotation = Quaternion.Euler(CarRotation);
     }
 }
